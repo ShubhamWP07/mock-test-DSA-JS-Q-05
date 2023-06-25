5c67c6b484a7db72277bcade9d19732a4f283d30
Shubham kumar <97464092+ShubhamWP07@users.noreply.github.com>
2023-06-25 13:19:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
+class Stack {
+  constructor() {
+    this.items = [];
+  }
+
+  // Push an element onto the stack
+  push(element) {
+    this.items.push(element);
+  }
+
+  // Pop the top element from the stack
+  pop() {
+    if (this.isEmpty()) {
+      return "Stack Underflow";
+    }
+    return this.items.pop();
+  }
+
+  // Check if the stack is empty
+  isEmpty() {
+    return this.items.length === 0;
+  }
+}
+
+// Usage example
+const stack = new Stack();
+
+stack.push(1);
+stack.push(2);
+stack.push(3);
+
+console.log(stack.pop()); // Output: 3
+console.log(stack.pop()); // Output: 2
+console.log(stack.isEmpty()); // Output: false
+console.log(stack.pop()); // Output: 1
+console.log(stack.isEmpty()); // Output: true
+console.log(stack.pop()); // Output: "Stack Underflow"
 
